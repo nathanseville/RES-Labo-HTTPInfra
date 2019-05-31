0c51d6c7462008c4355d1f72b8d14d770da164c1
nathan.seville <nathan.seville@hotmail.ch>
2019-05-31 14:44:03 +0200

    Dynamic http server

---
+++ docker-images/dynamic-http/src/index.js
@@ -0,0 +1,46 @@
+var Chance = require('chance');
+var chance = new Chance();
+
+var express = require('express');
+var app = new express();
+
+app.get('/', function(req, res) {
+    res.send(greatfunction());
+});
+
+app.listen(3000, function() {
+    console.log("Great App listenning on port 3000");
+});
+
+function greatfunction() {
+    var nbTrain = chance.integer({
+        min: 0,
+        max: 10
+    });
+
+    var trains = [];
+    var now = new Date(Date.now());
+
+    for(var i = 0; i < nbTrain; ++i) {
+        var track = chance.integer({
+            min: 1,
+            max: 7
+        });
+        var departure = chance.date({
+            day: now.getDate(),
+            month: now.getMonth(),
+            year: now.getYear()
+        });
+        var type = chance.pickone(['IR', 'ICN', 'S', 'IC', '-']);
+        var destination = chance.city();
+
+        trains.push({
+            track: track,
+            type: type,
+            departure: departure,
+            destination: destination
+        });
+    }
+
+    return trains;
+}
